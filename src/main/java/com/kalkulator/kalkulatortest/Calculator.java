e7c183838f8feaada50dd78e066e99afd9634207
LAPTOP-M4SHPO0H\dombe <dombermike@yahoo.com>
2018-10-16 20:28:52 +0200

    Kalkulator Commit

---
+++ src/main/java/com/kalkulator/kalkulatortest/Calculator.java
@@ -0,0 +1,11 @@
+package com.kalkulator.kalkulatortest;
+
+public class Calculator
+{
+    public void someMethod() {
+    }
+        public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+}
---
+++ src/main/java/com/kalkulator/kalkulatortest/KalkulatorTestApplication.java
@@ -0,0 +1,12 @@
+package com.kalkulator.kalkulatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalkulatorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalkulatorTestApplication.class, args);
+    }
+}
